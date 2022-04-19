61d6420896678027b5cb0de644dad2be89fa3869
yday <yangdayeon2@gmail.com>
2022-04-19 20:03:35 +0900

    checkbox를 switch로 바꾸기

--- app/src/main/java/kr/hs/emirim/s2106/mirim_project_0414_1/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2106/mirim_project_0414_1/MainActivity.java
@@ -10,10 +10,11 @@ import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioGroup;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
     RadioGroup rg;
-    CheckBox checkStart;
+    Switch checkStart;
     ImageView imgv;
     LinearLayout linearSub;
 
@@ -24,18 +25,16 @@ public class MainActivity extends AppCompatActivity {
 
         linearSub = findViewById(R.id.linear_sub);
         rg = findViewById(R.id.rg);
+        rg.setOnCheckedChangeListener(rgListener);
         checkStart = findViewById(R.id.check_start);
         imgv = findViewById(R.id.imgv);
 
-
-        Button btnDone = findViewById(R.id.btn_done);
-
-        btnDone.setOnClickListener(btnListener);
         checkStart.setOnCheckedChangeListener(checkListener);
     }
-    View.OnClickListener btnListener = new View.OnClickListener() {
-        public void onClick(View view) {
-            switch (rg.getCheckedRadioButtonId()) {
+    RadioGroup.OnCheckedChangeListener rgListener = new RadioGroup.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(RadioGroup radioGroup, int selectedId) {
+            switch (selectedId) {
                 case R.id.rb_dog:
                     imgv.setImageResource(R.drawable.dog);
                     break;
